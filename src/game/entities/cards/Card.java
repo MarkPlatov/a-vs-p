8417ef8a9da1e10b39e1e85bf107d08787d3cfbe
Mark Platov <mister.agamemnon@mail.ru>
2022-02-01 23:07:21 +0300

    interface Card modified

--- src/game/entities/cards/Card.java
+++ src/game/entities/cards/Card.java
@@ -3,7 +3,7 @@ package game.entities.cards;
 import game.entities.Entity;
 
 public interface Card extends Entity {
-	public void mix();
-	public void take();
-	public void use();
+	
+	void use();
+	
 }
--- src/game/entities/cards/CardsStack.java
+++ src/game/entities/cards/CardsStack.java
@@ -3,4 +3,7 @@ package game.entities.cards;
 import game.entities.Entity;
 
 public interface CardsStack extends Entity {
+	Card takeCard();
+	
+	void mix();
 }
